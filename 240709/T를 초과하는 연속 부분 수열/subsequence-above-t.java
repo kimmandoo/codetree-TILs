2a7fdb2c28f579dcfbf180a173a76f9b603131be
Mingyu Kim <mingyu5675@gmail.com>
2024-07-09 16:55:09 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 206ms, Memory: 13MB, Status: Passed - CodeTree

--- 240709/T를 초과하는 연속 부분 수열/subsequence-above-t.java
+++ 240709/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int t = sc.nextInt();
@@ -10,21 +9,16 @@ public class Main {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        // t보다 큰 수로만 이루어진 연속부분 수열 중 최대길이
+
         int max = 0;
-        int cnt = 1;
-        if(arr[0] < t) {
-            cnt = 0;
-        }else{
-            max = 1;
-        }
-        for(int i=1; i<n; i++){
-            if(arr[i-1]<arr[i] && arr[i] > t && arr[i-1] > t){
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] > t){
                 cnt++;
-            }else{
-                cnt = 1;
+                max = Math.max(max, cnt);
+            } else {
+                cnt = 0;
             }
-            max = Math.max(max, cnt);
         }
         System.out.print(max);
     }
