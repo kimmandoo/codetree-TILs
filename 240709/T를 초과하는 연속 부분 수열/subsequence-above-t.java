0d85d1b99116197be45c02db732c3b4ceec3047e
Mingyu Kim <mingyu5675@gmail.com>
2024-07-09 16:47:55 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240709/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        // t보다 큰 수로만 이루어진 연속부분 수열 중 최대길이
+        int max = 1;
+        int cnt = 1;
+        for(int i=1; i<n; i++){
+            if(arr[i-1]<arr[i] && arr[i] > t && arr[i-1] > t){
+                cnt++;
+            }else{
+                cnt = 1;
+            }
+            max = Math.max(max, cnt);
+        }
+        System.out.print(max);
+    }
+}
